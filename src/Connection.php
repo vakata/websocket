77d5aeadf72a76726ca8b21abe887c1cdcdf9cb6
Kevin Becker <k.becker@btr.zone>
2017-12-12 20:27:11 +0100

    initial connection class and server is abstracted

---
+++ src/Connection.php
@@ -0,0 +1,134 @@
+<?php
+declare(strict_types=1);
+
+namespace vakata\websocket;
+
+/**
+ * This class represents each connection we establish.
+ *
+ * @package vakata\websocket
+ */
+final class Connection
+{
+    use Base {
+        Base::send as private _send;
+        Base::sendClear as private;
+        Base::encode as private;
+        Base::receive as private;
+        Base::receiveClear as private;
+
+    }
+    protected $socket;
+    protected $header;
+    protected $request;
+    protected $cookies;
+
+    private $id;
+
+    /**
+     * Creates a new connection object with an ID.
+     *
+     * @param resource $socket  The socket that this connection represents.
+     * @param array    $header  Header data used on the connection handshake.
+     * @param string   $request Request given via GET.
+     * @param array    $cookies Given cookies from the user browser.
+     */
+    public function __construct($socket, array $header, string $request, array $cookies)
+    {
+        $this->id      = uniqid('', true);
+        $this->socket  = $socket;
+        $this->header  = $header;
+        $this->request = $request;
+        $this->cookies = $cookies;
+    }
+
+    /**
+     * Returns the socket this connection object represents.
+     *
+     * @return resource
+     */
+    public function getSocket()
+    {
+        return $this->socket;
+    }
+
+    /**
+     * Returns the connection ID.
+     *
+     * @return string
+     */
+    public function getID()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the host given by the handshake.
+     * Useful for validation.
+     *
+     * @return mixed|null
+     */
+    public function getHost()
+    {
+        return $this->header['host'] ?? null;
+    }
+
+    /**
+     * Returns the user agent given by the handshake.
+     * Useful for validation.
+     *
+     * @return mixed|null
+     */
+    public function getUserAgent()
+    {
+        return $this->header['user-agent'] ?? null;
+    }
+
+    /**
+     * Returns the origin given by the handshake.
+     * Useful for validation. (used to prevent Cross-Site WebSocket Hijacking (CSWSH)
+     *
+     * @return mixed|null
+     */
+    public function getOrigin()
+    {
+        return $this->header['origin'] ?? null;
+    }
+
+    /**
+     * Returns the extensions given by the handshake.
+     * Useful for validation.
+     *
+     * @return mixed|null
+     */
+    public function getExtensions()
+    {
+        return $this->header['sec-websocket-extensions'] ?? null;
+    }
+
+    /**
+     * Returns the request given by the handshake.
+     * Useful for serving the client different data based on the requested "channel".
+     *
+     * @return mixed|null
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    // TODO: add more getters
+
+
+    /**
+     * @param string $data   The data to send to the client.
+     * @param string $opcode The optional opcode to send in different formats.
+     * @param bool   $masked Whether the packet should be XOR-encoded to prevent attacks.
+     *
+     * @return bool Returns true if sending the message was successful.
+     */
+    public function send(string $data, string $opcode = 'text', bool $masked = false)
+    {
+        return $this->_send($this->socket, $data, $opcode, $masked);
+    }
+}
--- src/Server.php
+++ src/Server.php
@@ -8,16 +8,14 @@ namespace vakata\websocket;
  *
  * @package vakata\websocket
  */
-class Server
+abstract class Server
 {
     use Base;
 
-    protected $address = '';
-    protected $server = null;
-    protected $sockets = [];
-    protected $clients = [];
-    protected $callbacks = [];
-    protected $tick = null;
+    protected $address     = '';
+    protected $server      = null;
+    protected $sockets     = [];
+    protected $connections = [];
 
     /**
      * Create an instance.
@@ -46,9 +44,9 @@ class Server
         }
 
         $this->address = $address;
-        $ern = null;
-        $ers = null;
-        $this->server = stream_socket_server(
+        $ern           = null;
+        $ers           = null;
+        $this->server  = stream_socket_server(
             (in_array($addr['scheme'], ['wss', 'tls']) ? 'tls' : 'tcp') . '://' . $addr['host'] . ':' . $addr['port'],
             $ern,
             $ers,
@@ -69,53 +67,51 @@ class Server
     {
         $this->sockets[] = $this->server;
         while (true) {
-            if (isset($this->tick)) {
-                if (call_user_func($this->tick, $this) === false) {
-                    break;
-                }
+            if (!$this->onTick()) {
+                break;
             }
-            $changed = $this->sockets;
-            $write = [];
-            $except = [];
-            $changedNum = stream_select($changed, $write, $except, (isset($this->tick) ? 0 : null));
+            $changed    = $this->sockets;
+            $write      = [];
+            $except     = [];
+            $changedNum = stream_select($changed, $write, $except, 0);
+
             if (false === $changedNum) {
                 throw new WebSocketException('Could not select streams.');
             } elseif ($changedNum > 0) {
                 $messages = [];
-                foreach ($changed as $socket) {
+                foreach ($changed as $id => $socket) {
                     if ($socket === $this->server) {
                         $temp = stream_socket_accept($this->server);
                         if ($temp !== false) {
                             if ($this->connect($temp)) {
-                                if (isset($this->callbacks['connect'])) {
-                                    call_user_func($this->callbacks['connect'], $this->clients[(int)$temp], $this);
-                                }
+                                $this->onConnect($this->connections[$id]);
                             }
                         }
                     } else {
                         try {
-                            $message = $this->receive($socket);
+                            $message    = $this->receive($socket);
                             $messages[] = [
-                                'client' => $this->clients[(int)$socket],
-                                'message' => $message,
+                                'connection' => $this->connections[$id],
+                                'message'    => $message,
                             ];
                         } catch (WebSocketException $e) {
-                            if (isset($this->callbacks['disconnect'])) {
-                                call_user_func($this->callbacks['disconnect'], $this->clients[(int)$socket], $this);
-                            }
+                            $this->onDisconnect($this->connections[$id]);
                             $this->disconnect($socket);
                         }
                     }
                 }
                 foreach ($messages as $message) {
-                    if (isset($this->callbacks['message'])) {
-                        call_user_func($this->callbacks['message'], $message['client'], $message['message'], $this);
-                    }
+                    $this->onMessage($message['connection'], $message['message']);
                 }
             }
             usleep(5000);
         }
     }
+
+    /**
+     * @return bool
+     */
+    abstract protected function onTick(): bool;
 
     /**
      * Connects a socket.
@@ -140,7 +136,7 @@ class Server
         }
         $temp = [];
         foreach ($headers as $header) {
-            $header = explode(':', $header, 2);
+            $header                             = explode(':', $header, 2);
             $temp[trim(strtolower($header[0]))] = trim($header[1]);
         }
         $headers = $temp;
@@ -158,58 +154,80 @@ class Server
         if (isset($headers['cookie'])) {
             $temp = explode(';', $headers['cookie']);
             foreach ($temp as $v) {
-                $v = explode('=', $v, 2);
+                $v                    = explode('=', $v, 2);
                 $cookies[trim($v[0])] = $v[1];
             }
         }
-        $client = [
-            'socket' => $socket,
-            'headers' => $headers,
-            'resource' => $request[1],
-            'cookies' => $cookies,
-        ];
-        if (isset($this->callbacks['validate']) && !call_user_func($this->callbacks['validate'], $client, $this)) {
+        $client = new Connection($socket, $headers, $request[1], $cookies);
+        if (!$this->validateConnection($client)) {
             $this->sendClear($socket, "HTTP/1.1 400 Bad Request\r\n\r\n");
 
             return false;
         }
 
-        $response = [];
+        $response   = [];
         $response[] = 'HTTP/1.1 101 WebSocket Protocol Handshake';
         $response[] = 'Upgrade: WebSocket';
         $response[] = 'Connection: Upgrade';
         $response[] = 'Sec-WebSocket-Version: 13';
         $response[] = 'Sec-WebSocket-Location: ' . $this->address;
         $response[] = 'Sec-WebSocket-Accept: ' .
-            base64_encode(sha1($headers['sec-websocket-key'] . self::$magic, true));
+                      base64_encode(sha1($headers['sec-websocket-key'] . self::$magic, true));
         if (isset($headers['origin'])) {
             $response[] = 'Sec-WebSocket-Origin: ' . $headers['origin'];
         }
 
-        $this->sockets[(int)$socket] = $socket;
-        $this->clients[(int)$socket] = $client;
+        $this->connections[]             = $client;
+        $this->sockets[$client->getID()] = $socket;
 
         return $this->sendClear($socket, implode("\r\n", $response) . "\r\n\r\n");
     }
 
     /**
-     * Disconnects a socket.
+     * @param Connection $connection
      *
-     * @param resource $socket The socket to disconnect.
+     * @return bool
      */
-    protected function disconnect(&$socket)
+    abstract protected function validateConnection(Connection $connection): bool;
+
+    /**
+     * @param Connection $connection
+     *
+     * @return mixed
+     */
+    abstract protected function onConnect(Connection $connection);
+
+    /**
+     * @param Connection $connection
+     *
+     * @return mixed
+     */
+    abstract protected function onDisconnect(Connection $connection);
+
+    /**
+     * @param Connection $connection
+     */
+    public function disconnect(Connection $connection)
     {
-        unset($this->clients[(int)$socket], $this->sockets[(int)$socket], $socket);
+        unset($this->connections[$connection->getID()], $this->sockets[$connection->getID()]);
     }
 
     /**
-     * Get an array of all connected clients.
+     * @param Connection $connection
+     * @param string     $message
      *
-     * @return array     the clients
+     * @return mixed
      */
-    public function getClients(): array
+    abstract protected function onMessage(Connection $connection, string $message);
+
+    /**
+     * Get an array of all connections.
+     *
+     * @return array    the clients
+     */
+    public function getConnections(): array
     {
-        return $this->clients;
+        return $this->connections;
     }
 
     /**
@@ -220,95 +238,5 @@ class Server
     public function getServer()
     {
         return $this->server;
-    }
-
-    /**
-     * Set a callback to be executed when a client connects, returning `false` will prevent the client from connecting.
-     *
-     * The callable will receive:
-     *  - an associative array with client data
-     *  - the current server instance
-     * The callable should return `true` if the client should be allowed to connect or `false` otherwise.
-     *
-     * @param  callable $callback the callback to execute when a client connects
-     *
-     * @return $this
-     */
-    public function validateClient(callable $callback): Server
-    {
-        $this->callbacks['validate'] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Set a callback to be executed when a client is connected.
-     *
-     * The callable will receive:
-     *  - an associative array with client data
-     *  - the current server instance
-     *
-     * @param  callable $callback the callback to execute
-     *
-     * @return $this
-     */
-    public function onConnect(callable $callback): Server
-    {
-        $this->callbacks['connect'] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Set a callback to execute when a client disconnects.
-     *
-     * The callable will receive:
-     *  - an associative array with client data
-     *  - the current server instance
-     *
-     * @param  callable $callback the callback
-     *
-     * @return $this
-     */
-    public function onDisconnect(callable $callback): Server
-    {
-        $this->callbacks['disconnect'] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Set a callback to execute when a client sends a message.
-     *
-     * The callable will receive:
-     *  - an associative array with client data
-     *  - the message string
-     *  - the current server instance
-     *
-     * @param  callable $callback the callback
-     *
-     * @return $this
-     */
-    public function onMessage(callable $callback): Server
-    {
-        $this->callbacks['message'] = $callback;
-
-        return $this;
-    }
-
-    /**
-     * Set a callback to execute every few milliseconds.
-     *
-     * The callable will receive the server instance. If it returns boolean `false` the server will stop listening.
-     *
-     * @param  callable $callback the callback
-     *
-     * @return $this
-     */
-    public function onTick(callable $callback): Server
-    {
-        $this->tick = $callback;
-
-        return $this;
     }
 }
